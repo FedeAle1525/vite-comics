2f3ce14c950adf7edfcd7799a3bcc0808a96e803
FedeAle1525 <fedeale.1525@gmail.com>
2023-03-01 15:30:40 +0100

    VUE creation components

---
+++ vite-comics/src/components/AppFooter.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+  <footer>
+    FOOTER
+  </footer>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ vite-comics/src/components/AppMain.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+  <main>
+    MAIN
+  </main>
+</template>
+
+<style lang="scss" scoped></style>
