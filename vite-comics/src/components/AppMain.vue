6e0a97589d70bcf78856e5aede261d9ebf51ab7c
FedeAle1525 <fedeale.1525@gmail.com>
2023-03-01 16:32:21 +0100

    STYLE for component "AppMain"

--- vite-comics/src/components/AppMain.vue
+++ vite-comics/src/components/AppMain.vue
@@ -6,8 +6,18 @@ export default {
 
 <template>
   <main>
-    MAIN
+    <div class="container">
+      Content goes here
+    </div>
   </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use '../style/partials/variables' as *;
+
+main {
+  background-color: $bg-main;
+  color: white;
+  padding: 40px 0;
+}
+</style>
