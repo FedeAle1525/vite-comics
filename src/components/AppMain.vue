3d7a5a8e25058d10761143e74a0e5763d52e2f9d
FedeAle1525 <fedeale.1525@gmail.com>
2023-03-02 17:04:49 +0100

    FEAT dinamic elements in AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,83 @@
 <script>
 export default {
-
+  data() {
+    return {
+      comics: [
+        {
+          thumb: "/images/action-comics.jpg",
+          price: "$19.99",
+          series: "Action Comics",
+          type: "comic book"
+        },
+        {
+          thumb: "/images/american-vampire.jpg",
+          price: "$3.99",
+          series: "American Vampire 1976",
+          type: "comic book"
+        },
+        {
+          thumb: "/images/aquaman.jpg",
+          price: "$16.99",
+          series: "Aquaman",
+          type: "graphic novel"
+        },
+        {
+          thumb: "/images/batgirl.jpg",
+          price: "$2.99",
+          series: "Batgirl",
+          type: "comic book"
+        },
+        {
+          thumb: "/images/batman.jpg",
+          price: "$3.99",
+          series: "Batman",
+          type: "comic book"
+        },
+        {
+          thumb: "/images/batman-beyond.jpg",
+          price: "$2.99",
+          series: "Batman Beyond",
+          type: "comic book"
+        },
+        {
+          thumb: "/images/batman-superman.jpg",
+          price: "$3.99",
+          series: "Batman/Superman",
+          type: "comic book"
+        },
+        {
+          thumb: "/images/batman-superman-annual.jpg",
+          price: "$4.99",
+          series: "Batman/Superman Annual",
+          type: "comic book"
+        },
+        {
+          thumb: "/images/batman-joker-warzone.jpg",
+          price: "$5.99",
+          series: "Batman: The Joker War Zone",
+          type: "comic book"
+        },
+        {
+          thumb: "/images/batman-three-joker.jpg",
+          price: "$6.99",
+          series: "Batman: Three Jokers",
+          type: "comic book"
+        },
+        {
+          thumb: "/images/harley-queen.jpg",
+          price: "$4.99",
+          series: "Batman: White Knight Presents: Harley Quinn",
+          type: "comic book"
+        },
+        {
+          thumb: "/images/catwoman.jpg",
+          price: "$16.99",
+          series: "Catwoman",
+          type: "graphic novel"
+        }
+      ]
+    }
+  }
 }
 </script>
 
@@ -8,67 +85,11 @@ export default {
   <main>
     <div class="container">
       <div class="row">
-        <div class="col-6">
+        <div class="col-6" v-for="comic in comics">
           <div class="card">
-            <img src="/images/action-comics.jpg">
+            <img :src="comic.thumb">
 
-            <span>Titolo</span>
-          </div>
-        </div>
-
-        <div class="col-6">
-          <div class="card">
-            <img src="/images/action-comics.jpg">
-
-            <span>Titolo</span>
-          </div>
-        </div>
-
-        <div class="col-6">
-          <div class="card">
-            <img src="/images/action-comics.jpg">
-
-            <span>Titolo</span>
-          </div>
-        </div>
-
-        <div class="col-6">
-          <div class="card">
-            <img src="/images/action-comics.jpg">
-
-            <span>Titolo</span>
-          </div>
-        </div>
-
-        <div class="col-6">
-          <div class="card">
-            <img src="/images/action-comics.jpg">
-
-            <span>Titolo</span>
-          </div>
-        </div>
-
-        <div class="col-6">
-          <div class="card">
-            <img src="/images/action-comics.jpg">
-
-            <span>Titolo</span>
-          </div>
-        </div>
-
-        <div class="col-6">
-          <div class="card">
-            <img src="/images/action-comics.jpg">
-
-            <span>Titolo</span>
-          </div>
-        </div>
-
-        <div class="col-6">
-          <div class="card">
-            <img src="/images/action-comics.jpg">
-
-            <span>Titolo</span>
+            <span>{{ comic.series }}</span>
           </div>
         </div>
       </div>
@@ -105,6 +126,7 @@ main {
       aspect-ratio: 1/1;
       object-fit: cover;
       object-position: top;
+      display: block;
     }
 
     span {
