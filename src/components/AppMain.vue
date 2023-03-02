13722f335869c6e94d32b6fb0f33c0d555224364
FedeAle1525 <fedeale.1525@gmail.com>
2023-03-02 16:27:42 +0100

    FEAT static structure for AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -7,7 +7,71 @@ export default {
 <template>
   <main>
     <div class="container">
-      Content goes here
+      <div class="row">
+        <div class="col-6">
+          <div class="card">
+            <img src="/images/action-comics.jpg">
+
+            <span>Titolo</span>
+          </div>
+        </div>
+
+        <div class="col-6">
+          <div class="card">
+            <img src="/images/action-comics.jpg">
+
+            <span>Titolo</span>
+          </div>
+        </div>
+
+        <div class="col-6">
+          <div class="card">
+            <img src="/images/action-comics.jpg">
+
+            <span>Titolo</span>
+          </div>
+        </div>
+
+        <div class="col-6">
+          <div class="card">
+            <img src="/images/action-comics.jpg">
+
+            <span>Titolo</span>
+          </div>
+        </div>
+
+        <div class="col-6">
+          <div class="card">
+            <img src="/images/action-comics.jpg">
+
+            <span>Titolo</span>
+          </div>
+        </div>
+
+        <div class="col-6">
+          <div class="card">
+            <img src="/images/action-comics.jpg">
+
+            <span>Titolo</span>
+          </div>
+        </div>
+
+        <div class="col-6">
+          <div class="card">
+            <img src="/images/action-comics.jpg">
+
+            <span>Titolo</span>
+          </div>
+        </div>
+
+        <div class="col-6">
+          <div class="card">
+            <img src="/images/action-comics.jpg">
+
+            <span>Titolo</span>
+          </div>
+        </div>
+      </div>
     </div>
   </main>
 </template>
@@ -19,5 +83,33 @@ main {
   background-color: $bg-main;
   color: white;
   padding: 40px 0;
+}
+
+.row {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0 -10px;
+  row-gap: 30px;
+
+  .col-6 {
+    flex-basis: calc(100% / 6);
+    padding: 0 10px;
+  }
+
+  .card {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+
+    img {
+      aspect-ratio: 1/1;
+      object-fit: cover;
+      object-position: top;
+    }
+
+    span {
+      text-transform: uppercase;
+    }
+  }
 }
 </style>
